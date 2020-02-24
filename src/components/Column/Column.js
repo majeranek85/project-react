36f721ab87543756ab4ba7c7f3f7d9385a923245
Marta Majerowska <majeranek85@gmail.com>
2020-02-24 20:31:08 +0100

    remove import

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
-import {settings, listData} from '../../data/dataStore.js';
+import {settings} from '../../data/dataStore.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
