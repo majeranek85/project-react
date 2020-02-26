431dbb70ef2d87ef7dc1b12a00d01572d06da2fc
Marta Majerowska <majeranek85@gmail.com>
2020-02-26 17:13:15 +0100

    Fix card filtering bug

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import Column from './Column.js';
 
-export const getCardsForColumn = ({cards}, listId) => cards.filter(card => card.listId == listId);
+export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForColumn(state, props.id),
